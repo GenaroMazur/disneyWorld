e5ac6660624c7b49c78f9fe9d914c3b9897ff9d2
genaro Mazur <genaromazur03@gmail.com>
2022-09-30 22:24:07 -0300

    [ add : auth.routes, characters.routes, movies.routes || mod : app.js, index.routes ]

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
+const session = require("express-session");
+app.use(session({
+    secret:"ariel",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("The Disney World is run in the port :" + PORT);
+});
+
+const indexRoutes = require("./routes/index.routes")
+app.use("/", indexRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).json("URL not found (404 )");
+});
---
+++ routes/index.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = express.Router();
+
+const mainController = require("./../controllers/mainController");
+routes.get("/", mainController);
+
+const authRoute = require("./auth.routes");
+routes.use("/auth", authRoute);
+
+const charactersRoute = require("./characters.routes");
+routes.use("/characters", charactersRoute);
+
+const moviesRoute = require("./movies.routes");
+routes.use("/movies", moviesRoute);
+
+module.exports = routes;
